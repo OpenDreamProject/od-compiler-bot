ab48abce72b7525e14a43eb859b46e6186a725c7
Crossedfall <ping@crossedfall.io>
2023-06-03 03:32:06 -0500

    Finishes out docker tests

--- tests/docker_actions/test_docker.py
+++ tests/docker_actions/test_docker.py
@@ -1,16 +1,50 @@
-import shutil
 from os import chdir
 
 import pytest
+from gitdb.exc import BadName
 
+from docker.errors import BuildError
 from od_compiler.util.docker_actions import compileOD
+from od_compiler.util.docker_actions import updateBuildImage
 
 
-@pytest.mark.order("last")
-def test_image_bulder(build_dir):
-    shutil.copytree("docker", f"{build_dir}/docker")
+@pytest.mark.order(index=-1)
+def test_standard_compile(build_dir):
     code = 'world.log << "Hello, pytest!"'
 
     chdir(build_dir)
     test_output = compileOD(codeText=code, compile_args=[""], timeout=30)
     assert test_output.keys() >= {"compiler", "server", "timeout"}
+
+
+@pytest.mark.order(index=-2)
+def test_build_error(build_dir, mocker):
+    mocker.patch(
+        "od_compiler.util.docker_actions.updateBuildImage", side_effect=BuildError(reason="pytest", build_log="pytest")
+    )
+    code = 'world.log << "Hello, pytest!"'
+
+    chdir(build_dir)
+    test_output = compileOD(codeText=code, compile_args=[""], timeout=30)
+    assert "build_error" in test_output.keys()
+
+
+@pytest.mark.order(index=-3)
+def test_compile_timeout(build_dir):
+    code = 'world.log << "Sleeping"\nsleep(500)'
+
+    chdir(build_dir)
+    test_output = compileOD(codeText=code, compile_args=[""], timeout=5)
+    assert test_output["timeout"] is True
+
+
+@pytest.mark.order(index=-4)
+def test_compile_bad_logs(build_dir, mocker):
+    logs_return = {"error": "pytest"}
+    mocker.patch("od_compiler.util.docker_actions.splitLogs", return_value=logs_return)
+
+    chdir(build_dir)
+    code = 'world.log << "Hello, pytest!"'
+    test_output = compileOD(codeText=code, compile_args=[""], timeout=30)
+
+    assert "error" in test_output.keys()
